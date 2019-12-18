98b38e9bb96a9b8a55216b30a38db58af330c3fb
henrique <henrique.gaia@gmail.com>
2019-12-18 10:40:21 +0100

    Bring the mux

--- main.go
+++ main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
-	arithmetic "github.com/henrique/cobraStuff/arithmetic"
+	// "str/conv"
+	// "math/rand"
+	// "encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println(arithmetic.Ints(1,2,3))
+	r := mux.NewRouter()
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":3000", r))
+
 	// http.HandleFunc("/", index)
 	// http.HandleFunc("/about", about)
 	// fmt.Println("server starting ...")
 	// http.ListenAndServe(":3000", nil)
+}
+
+type Book struct {
+	ID     string  `json:"id"`
+	ISBN   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
